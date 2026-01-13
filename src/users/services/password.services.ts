3673ff461c8ca9e80845fb656dd9afc99650731c
Kaybhee <abdulkabirbadru@gmail.com>
2026-01-13 22:28:23 +0100

    hashing password using bcrypt

--- src/users/services/password.services.ts
+++ src/users/services/password.services.ts
@@ -1,12 +1,10 @@
-import bcrypt from 'bcrypt'
-
-const genSalt = 10;
+import * as bcrypt from 'bcrypt'
 
 export async function hashPass(password: string) : Promise<string> {
     const salt = await bcrypt.genSalt(10)
-    return await salt.hash(password, salt) 
+    return await bcrypt.hash(password, salt) 
 }
 
-export async function verifyUserPassword(password: string, hashPassword: string): Promise<string> {
+export async function verifyUserPassword(password: string, hashPassword: string): Promise<boolean> {
     return await bcrypt.compare(password, hashPassword)
 }
