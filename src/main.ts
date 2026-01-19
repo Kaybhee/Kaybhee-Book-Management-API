57336090b25c28a70a83de3235a02941eece5a74
Kaybhee <abdulkabirbadru@gmail.com>
2026-01-19 16:24:59 +0100

    setting validation pipes globally for this app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { number } from 'joi';
-
+import { ValidationPipe } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(
+    new ValidationPipe ({
+      whitelist : true,
+      forbidNonWhitelisted : true,
+      transform : true,
+      disableErrorMessages : false
+    })
+  )
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT');
   await app.listen(Number(port), () => {
