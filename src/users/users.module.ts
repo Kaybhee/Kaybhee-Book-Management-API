9468a14a6578ae58e79cc348b0a53e3d51bb1aa5
Kaybhee <abdulkabirbadru@gmail.com>
2026-02-01 15:42:02 +0100

    user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,16 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/Entity/db.entity';
+import { User } from 'src/Entity/db.userEntity';
 import { SendMailService } from './services/mail.services';
-import { signUser } from './services/token.services';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global : true,
+      secret : process.env.JWT_SECRET,
+      signOptions: { expiresIn: "60d"}
+    })
   ],
-  providers: [UsersService, SendMailService, signUser],
+  exports: [UsersService],
+  providers: [UsersService, SendMailService],
   controllers : [UsersController],
 })
 export class UsersModule {}
