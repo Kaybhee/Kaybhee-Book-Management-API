f1fed3aee4852e3232382370453d79482192277c
Kaybhee <abdulkabirbadru@gmail.com>
2026-02-02 20:04:14 +0100

    included severeal fields in the entity

--- src/Entity/db.bookEntity.ts
+++ src/Entity/db.bookEntity.ts
@@ -3,23 +3,30 @@ import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeor
 @Entity()
 export class Books {
     @PrimaryGeneratedColumn()
-    bookId: number
+    id: number
 
-    @Column({ length: 10})
-    ISSN: string
+    @Column()
+    Title: string
+
+    @Column()
+    Publisher: string
+
+    @Column()
+    Edition: string
+
+    @Column()
+    Genre: string
 
     @Column( {length: 13})
     ISBN: string
 
     @Column()
-    bookName: string
-
-    @Column()
     Author: string
-
-    @CreateDateColumn()
-    producedDate: Date
 
     @Column({ default: false })
     isAvailable: boolean
+
+    @CreateDateColumn()
+    publishingDate: Date
+ 
 }
