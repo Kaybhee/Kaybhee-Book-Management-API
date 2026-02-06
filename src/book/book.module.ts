9dbc7531e7a5486da5fbe06b23690fbcb36035fa
Kaybhee <abdulkabirbadru@gmail.com>
2026-02-06 17:04:27 +0100

    book module

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,12 +3,15 @@ import { BookService } from './book.service';
 import { BookController } from './book.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Books } from 'src/Entity/db.bookEntity';
+import { Borrow } from 'src/Entity/db.borrowEntity';
+import { User } from 'src/Entity/db.userEntity';
+import { SendMailService } from 'src/users/services/mail.services';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Books])
+    TypeOrmModule.forFeature([Books, Borrow, User])
   ],
-  providers: [BookService],
+  providers: [BookService, SendMailService],
   controllers: [BookController]
 })
 export class BookModule {}
