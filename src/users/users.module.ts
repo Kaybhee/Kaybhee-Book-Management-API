28476a127f0690ee114ec3e22f1d1a157d150237
Kaybhee <abdulkabirbadru@gmail.com>
2026-02-08 09:03:25 +0100

    addition of more entities and providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,18 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/Entity/db.userEntity';
 import { SendMailService } from './services/mail.services';
 import { JwtModule } from '@nestjs/jwt';
+import { BookService } from 'src/book/book.service';
+import { Borrow } from 'src/Entity/db.borrowEntity';
+import { Books } from 'src/Entity/db.bookEntity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Books, Borrow]),
     JwtModule.register({
       global : true,
       secret : process.env.JWT_SECRET,
       signOptions: { expiresIn: "60d"}
-    })
+    }),
   ],
   exports: [UsersService],
-  providers: [UsersService, SendMailService],
+  providers: [UsersService, SendMailService, BookService],
   controllers : [UsersController],
 })
 export class UsersModule {}
